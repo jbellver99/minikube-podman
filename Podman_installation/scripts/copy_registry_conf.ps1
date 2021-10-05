330b351220057523ba753724b266a63994e46dd6
ultymatom <90399827+ultymatom@users.noreply.github.com>
2021-10-05 12:59:37 +0200

    Update copy_registry_conf.ps1

--- Podman_installation/scripts/copy_registry_conf.ps1
+++ Podman_installation/scripts/copy_registry_conf.ps1
@@ -1,4 +1,3 @@
 cmd /c "Icacls C:\\Users\\$($env:USERNAME)\\.minikube\\machines\\minikube\\id_rsa /Inheritance:r"
-cmd /c 'Icacls C:\\Users\\$($env:USERNAME)\\.minikube\\machines\\minikube\\id_rsa /Grant:r "%Username%":"(R)"'
 scp -i $(minikube ssh-key) -o StrictHostKeyChecking=no C:\Users\$($env:USERNAME)\Downloads\podman-2.2.1\registries.conf docker@$(minikube ip):/home/docker/registries.conf
 minikube ssh "dos2unix  /home/docker/registries.conf;sudo cp /home/docker/registries.conf /etc/containers/registries.conf"
